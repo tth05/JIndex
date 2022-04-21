68377f5b5ad27bd1518f4bd953bb452fe2babe0a
tth05 <36999320+tth05@users.noreply.github.com>
2022-04-21 21:26:09 +0200

    v0.0.26

--- src/main/java/com/github/tth05/jindex/ClassIndex.java
+++ src/main/java/com/github/tth05/jindex/ClassIndex.java
@@ -11,7 +11,7 @@ public class ClassIndex extends ClassIndexChildObject {
     static {
         try {
             boolean isDev = false;
-            Path tempFilePath = Paths.get(System.getProperty("java.io.tmpdir")).resolve("jindex_lib_0.0.25.dll");
+            Path tempFilePath = Paths.get(System.getProperty("java.io.tmpdir")).resolve("jindex_lib_0.0.26.dll");
             if (isDev || !Files.exists(tempFilePath)) {
                 Files.copy(ClassIndex.class.getResourceAsStream("/jindex_rs.dll"), tempFilePath, StandardCopyOption.REPLACE_EXISTING);
             }
